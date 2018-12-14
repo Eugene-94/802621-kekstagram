59260b31086630c69ed8b0eedfc899b2f8edb8f6
Евгений Кайгородов <gekak94@gmail.com>
2018-12-14 21:52:49 +0600

    Ajax-запросы, рефакторинг

--- js/picture.js
+++ js/picture.js
@@ -6,7 +6,7 @@
     @function
     @param {object} destinationContainer - адрес размещения изображений
   */
-  function insertPhotoMiniatures(destinationContainer) {
+  /*function insertPhotoMiniatures(destinationContainer) {
     for (var j = 0; j < window.photoList.length; j++) {
       var photoItem = photoTemplate.cloneNode(true);
       photoItem.querySelector('.picture__img').src = window.photoList[j].url;
@@ -16,10 +16,30 @@
 
       destinationContainer.appendChild(photoItem);
     }
-  }
+  }*/
+
+  var successRender = function (data) {
+    for (var j = 0; j < data.length; j++) {
+      var photoItem = photoTemplate.cloneNode(true);
+      photoItem.querySelector('.picture__img').src = data[j].url;
+      photoItem.querySelector('.picture__img').id = data[j].id;
+      photoItem.querySelector('.picture__comments').textContent = data[j].comments.length;
+      photoItem.querySelector('.picture__likes').textContent = data[j].likes;
+
+      picturesContainer.appendChild(photoItem);
+    }
+  };
+
+  var errorRender = function (errorMessage) {
+    var errorTemplate = document.querySelector('#error');
+    var errorMessage = errorTemplate.cloneNode(true);
+
+    //node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', errorMessage);
+  };
 
   var photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var picturesContainer = document.querySelector('.pictures');
 
-  insertPhotoMiniatures(picturesContainer);
+  window.downloading(successRender, errorRender);
 })();
