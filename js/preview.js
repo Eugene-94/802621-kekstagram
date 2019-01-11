ba639691f67a395c069aa351532fec0536cc1368
Евгений Кайгородов <gekak94@gmail.com>
2019-01-11 21:56:56 +0600

    Рефакторинг кода

--- js/preview.js
+++ js/preview.js
@@ -4,6 +4,8 @@
   var COMMENTS_SLICE = 5;
 
   var Image = function () {
+    this.state = 1;
+
     this.open = function () {
       bigPicture.classList.remove('hidden');
     };
@@ -55,9 +57,28 @@
     this.commentsCounter = function (currentComment, commentsAmount) {
       bigPicture.querySelector('.social__comment-count').innerHTML = currentComment + ' из <span class="comments-count">' + commentsAmount + '</span> комментариев';
     };
-
-    this.state = 1;
   };
+
+  function openImage(order) {
+    var image = new Image();
+    image.open();
+    image.render(order);
+    image.renderComments(order);
+
+    window.loadComments = function () {
+      var multiplier = Math.ceil((window.serverData[order - 1].comments.length - COMMENTS_SLICE) / COMMENTS_SLICE);
+
+      if (image.state <= multiplier) {
+        image.insertComments(window.serverData[order - 1].comments, image.state * COMMENTS_SLICE);
+        image.state += 1;
+      }
+    };
+  }
+
+  function closeImage() {
+    var image = new Image();
+    image.close();
+  }
 
   var bigPicture = document.querySelector('.big-picture');
   var currentCommentsList = bigPicture.querySelector('.social__comments');
@@ -68,27 +89,18 @@
   imagesContainer.addEventListener('click', function (evt) {
     if (evt.target.classList.contains('picture__img')) {
       var orderNumber = window.tools.extractNum(evt.target.getAttribute('src'));
-      var image = new Image();
-      image.open();
-      image.render(orderNumber);
-      image.renderComments(orderNumber);
+      openImage(orderNumber);
 
-      loadCommentsBtn.addEventListener('click', function () {
-        var multiplier = Math.ceil((window.serverData[orderNumber - 1].comments.length - COMMENTS_SLICE) / COMMENTS_SLICE);
-
-        if (image.state <= multiplier) {
-          image.insertComments(window.serverData[orderNumber - 1].comments, image.state * COMMENTS_SLICE);
-          image.state += 1;
-        }
-      });
+      loadCommentsBtn.addEventListener('click', window.loadComments);
     }
   });
 
   var bigPictureCancel = document.querySelector('.big-picture__cancel');
 
   bigPictureCancel.addEventListener('click', function () {
-    var image = new Image();
-    image.close();
+    closeImage();
+
+    loadCommentsBtn.removeEventListener('click', window.loadComments);
   });
 
   bigPicture.addEventListener('click', function (evt) {
@@ -100,32 +112,22 @@
 
   bigPicture.addEventListener('keydown', function (evt) {
     if (evt.code === 'Escape') {
-      var image = new Image();
-      image.close();
+      closeImage();
+
+      loadCommentsBtn.removeEventListener('click', window.loadComments);
     }
   });
 
   imagesContainer.addEventListener('keydown', function (evt) {
     if (evt.code === 'Enter') {
       var orderNumber = window.tools.extractNum(evt.target.querySelector('.picture__img').getAttribute('src'));
-      var image = new Image();
-      image.open();
-      image.render(orderNumber);
-      image.renderComments(orderNumber);
+      openImage(orderNumber);
 
-      loadCommentsBtn.addEventListener('click', function () {
-        var multiplier = Math.ceil((window.serverData[orderNumber - 1].comments.length - COMMENTS_SLICE) / COMMENTS_SLICE);
-
-        if (image.state <= multiplier) {
-          image.insertComments(window.serverData[orderNumber - 1].comments, image.state * COMMENTS_SLICE);
-          image.state += 1;
-        }
-      });
+      loadCommentsBtn.addEventListener('click', window.loadComments);
     }
 
     if (evt.code === 'Escape') {
-      image = new Image();
-      image.close();
+      closeImage();
     }
   });
 })();
